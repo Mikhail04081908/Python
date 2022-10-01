baa3ac621b75600135f2c065d88abbe3c6003700
Kaluzhin <fk2090@gmail.com>
2022-10-01 14:49:03 +0300

    Lesson 1

---
+++ lesson_1/hello.py
@@ -0,0 +1,75 @@
+# типы данных и переменная 
+# int, float, boolean, str, list, None
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 12334
+# print(type(value))
+# s = 'hello world'
+# print(s) # вывод строки
+
+ # print(a, b, s)
+# print(a, ' - ', b, ' - ', s)
+
+# print(f'{a} - {b} - {s}')
+
+# f = False
+
+# print(f)
+
+# list = ['1','2','3', 'hello', 1,2,3,4,5,6,7, True]
+# print (list)
+
+# print('Введите a');
+# a = float(input())
+# print('Введите b');
+# b = int (input())
+# print(a, ' + ' , b, ' = ', a+b)
+
+
+
+# Арифметические операции
+
+# a = 1.312121212212
+# b = 3
+# c = round(a * b, 7)
+# print(c)
+
+# # '//' деление до целый
+# # '/' деление с остатком
+# f = [1,2,3,4]
+# print(f)
+
+# is_odd = not f[0] % 2
+# print(is_odd)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a>b:
+#     print(b)
+# else:
+#     print(b)
+
+# Цикл
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print('Пожалуй')
+#     print('хватит')
+# print(inverted)
+
+# for
+
+# for i in 1,2,3,4,5:
+#     print(i**2)
+
+# r = range(10)
+# for i in range(1, 10 ,2):
+#     print(i)
+
+# help(int)
